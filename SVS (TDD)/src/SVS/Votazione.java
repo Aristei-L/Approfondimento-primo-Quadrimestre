4b1bb9482ed1151e7a63e769873cf66e0a4ed30f
Aristei-L <aristei.lorenzo@studenti.salesianisesto.it>
2025-02-24 10:20:11 +0100

    Quarto Commit
    Aggiunta Metodi Classe Votazione

--- SVS (TDD)/src/SVS/Votazione.java
+++ SVS (TDD)/src/SVS/Votazione.java
@@ -14,4 +14,51 @@ public class Votazione {
         this.limiteVotiTotali = limiteVotiTotali;
         votiTotali = 0;
     }
+    public void aggiungiCanzone(Canzone canzone) {
+        canzoni.add(canzone);
+    }
+    public void aggiungiVotante(Votante votante) { votanti.add(votante); }
+
+    public void registraVoto(Votante votante, String titoloCanzone, int voto) {
+        Canzone canzone = trovaCanzone(titoloCanzone);
+        if (votiTotali < limiteVotiTotali) {
+            if (canzone.getVoto() == -1) {
+                Votante votanteEsistente = trovaVotante(votante.getNome());
+                if (votanteEsistente != null) {
+
+                    votanteEsistente.vota(canzone, voto);
+                } else {
+                    votante.vota(canzone, voto);
+                    votanti.add(votante);
+                    votiTotali++;
+                }
+            }
+        }
+    }
+    public Canzone trovaCanzone(String titolo) {
+        for (Canzone c : canzoni) {
+            if (c.getTitolo().equalsIgnoreCase(titolo)) {
+                return c;
+            }
+        }
+        return null;
+    }
+    public Votante trovaVotante(String Nome){
+        for (Votante c : votanti) {
+            if (c.getNome().equalsIgnoreCase(Nome)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Canzone> getCanzoni() {
+        return canzoni;
+    }
+
+    // Getter per i votanti
+    public ArrayList<Votante> getVotanti() {
+        return votanti;
+    }
 }
+
--- SVS (TDD)/src/test/TestVotazione.java
+++ SVS (TDD)/src/test/TestVotazione.java
@@ -1,51 +1,52 @@
 package test;
 import SVS.*;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-public class TestVotazione {
-    static Votazione votazione;
 
-    @BeforeAll
+public class TestVotazione {
+    Votazione votazione;
+
+    @BeforeEach
     @DisplayName("Settings")
-    static void setup(){
+    void setup(){
         votazione = new Votazione(5);
+        votazione.aggiungiVotante(new Votante("Mario Rossi", 5));
+        votazione.aggiungiCanzone(new Canzone("Il Pescatore", "Fabrizio De Andrè"));
     }
     @Test
     @DisplayName("Test per aggiungere una canzone alla lista")
     void AggiungiCanzoneTest(){
-        votazione.aggiungiCanzone(new Canzone("Il Pescatore", "Fabrizio De Andrè"));
-        assertTrue(votazione.getCanzoni().getLast().equals(new Canzone("Il Pescatore", "Fabrizio De Andrè")));
+        votazione.aggiungiCanzone(new Canzone("Bocca di rosa", "Fabrizio De Andrè"));
+        assertTrue(votazione.trovaCanzone("Bocca di rosa").equals(votazione.getCanzoni().getLast()));
     }
     @Test
     @DisplayName("Test per aggiungere un votante alla lista")
-    void AggiungiCanzoneTest(){
-        votazione.aggiungiVotante(new Votante("Mario Rossi", 5));
-        assertTrue(votazione.getVotanti().getLast().equals(new Votante("Mario Rossi", 5)));
+    void AggiungiVotanteTest(){
+        votazione.aggiungiVotante(new Votante("Marco Verdi", 3));
+        assertTrue(votazione.getVotanti().getLast().equals(votazione.trovaVotante("Marco Verdi")));
     }
     @Test
     @DisplayName("Test per Trovare una Canzone")
     void TrovaCanzoneTest(){
         Canzone canzone = votazione.trovaCanzone("Il Pescatore");
-        assertTrue(canzone.equals(new Canzone("Il Pescatore", "Fabrizio De Andrè")));
+        assertTrue(canzone.equals(votazione.getCanzoni().getLast()));
     }
     @Test
     @DisplayName("Test per Trovare un votante")
-    void TrovaCanzoneTest(){
+    void TrovaVotanteTest(){
         Votante votante = votazione.trovaVotante("Mario Rossi");
-        assertTrue(votante.equals(new Votante("Mario Rossi", 5)));
+        assertTrue(votante.equals(votazione.getVotanti().getLast()));
     }
     @Test
     @DisplayName("Test per Registrare un Voto")
     void RegistraVotoTest(){
-        registraVoto(new Votante("Mario Rossi 5",5),"Il Pescatore",9);
+        votazione.registraVoto(new Votante("Mario Rossi 5",5),"Il Pescatore",9);
         Votante votante = votazione.trovaVotante("Mario Rossi");
         Canzone canzone = votazione.trovaCanzone("Il Pescatore");
-        assertTrue(votante.equals(new Votante("Mario Rossi", 4)));
+        assertTrue(votante.equals());
         assertTrue(canzone.equals(new Canzone("Il Pescatore", "Fabrizio De Andrè")));
     }
 
